2fe3749027ed58f5054919c85d2d046df10a1689
Yashpreet Singh <yashpreet1686@gmail.com>
2024-10-02 14:10:51 +0530

    useGSAP and useRef

--- GSAPusingReact/src/App.jsx
+++ GSAPusingReact/src/App.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React, { useRef } from 'react'
 
 const App = () => {
+
+  const containerRef = useRef() ;
+
+  useGSAP(()=>{
+    gsap.from(".box", {
+      rotate: 540,
+      duration: 1.5,
+      delay: 0.5,
+      scale: 0
+    })
+  }, {scope: containerRef })
+
+
   return (
     <main>
-      <div id="box">
-        
+      <div className="container" ref={containerRef}>
+        <div className="circle"></div>
+        <div className="box"></div>
+      </div>
+      <div className="kuchh">
+        <div className="circle"></div>
+        <div className="box"></div>
       </div>
     </main>
   )
